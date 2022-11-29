1f0af669cf603f3477b876690fd83da24c4f1b4f
Rahmadfaisizarwibakti <113441229+Rahmadfaisizarwibakti@users.noreply.github.com>
2022-11-29 23:05:42 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger');
+const hilang = document.querySelector('.hilang');
+
+hamburger.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
+});
